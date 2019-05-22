823eaa6fb179f30e40c79a5d3f66a6ec4175a831
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 04:41:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
  array.delete(string)
 end
 
-def using_delete_at(array, interger)
- array.delet_at(interger)
+def using_delete_at(array, integer)
+ array.delet_at(integer)
 end
